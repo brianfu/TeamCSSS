1ed32b8c7c337e524c82d258c703db18f7fd6e28
Ari <ari.blondal@gmail.com>
2017-02-10 22:15:43 -0800

    First commit by Ari, Core and first funcs added, as well as Main

---
+++ Core/load_sound.py
@@ -0,0 +1,12 @@
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer:
+        return NoneSound()
+    fullname = os.path.join('data', name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error, message:
+        print 'Cannot load sound:', wav
+        raise SystemExit, message
+    return sound
---
+++ main.py
@@ -0,0 +1,12 @@
+import os, sys
+import pygame
+from pygame.locals import *
+import "Core/load_sound"
+import "Core/load_image"
+
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
+
+
+
+
