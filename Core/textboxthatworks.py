6d1f0b84deab3d9ae0a9c8dba252e1eef06fa830
brianfu <Fu.Brian@outlook.com>
2017-02-11 13:32:12 -0800

    bingo i think

---
+++ Core/textboxthatworks.py
@@ -0,0 +1,41 @@
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+class textbox(object):
+    def __init__(self, screen):
+        self.screen = screen
+        self.x_pos = 0
+        self.y_pos = 31
+        self.x_offset = 30
+        self.y_offset = 30
+        self.font = pygame.font.SysFont('Calibri', 25, False, False)
+        self.rendered_text = ['','','','','']
+        
+    def create_textbox(self):
+        pygame.draw.rect(self.screen, RED, [self.x_pos*self.x_offset, self.y_pos*self.y_offset, 24*self.x_offset, 5*self.y_offset], 10) 
+        
+    def line1(self, text):
+        self.rendered_text[0] = self.font.render(str(text), True, BLACK)
+    
+    def line2(self, text):
+        self.rendered_text[1] = self.font.render(str(text), True, BLACK)
+        
+    def line3(self, text):
+        self.rendered_text[2] = self.font.render(str(text), True, BLACK)
+        
+    def line4(self, text):
+        self.rendered_text[3] = self.font.render(str(text), True, BLACK)
+        
+    def line5(self, text):
+        self.rendered_text[4] = self.font.render(str(text), True, BLACK)
+    
+    def blitz(self):
+        for i in range (5):
+            self.screen.blit(self.rendered.text[i], [self.x_pos*self.x_offset, self.y_pos*self.y_offset])
+            self.y_pos += 1
+        
